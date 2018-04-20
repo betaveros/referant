34eaf9509c4c233dde60c9338f281f9e907f68fc
Max Murin <maxmurin@mit.edu>
2018-04-19 20:21:38 -0400

    added color filters thing

--- referant.ts
+++ referant.ts
@@ -37,3 +37,44 @@ $('.layout-img').resizable({
 		'se': '.ui-resizable-se',
 	}
 });
+let colorVisible = false;
+$('#color-activate').click(function () {
+	colorVisible = !colorVisible;
+    if (colorVisible) {
+        $('#color-select').show();
+    } else {
+        $('#color-select').hide();
+    }
+});
+
+const filterNames : string[] = ["red", "yellow", "blue", "green", "purple", "orange", "white", "gray", "black"];
+
+let activeFilters = new Set();
+
+$(document).ready(function () {
+	for(const filter of filterNames) {
+		const button = $('.' + filter + '-selector');
+		button.click(function () {
+			makeFilter(filter);
+		});
+	}
+});
+
+function makeFilter(name: string) {
+	console.log(activeFilters);
+	if(!activeFilters.has(name)) {
+		activeFilters.add(name);
+		const thing = $(document.createElement('div'));
+		const x = $(document.createElement('button'));
+		x.text('x');
+		const writing = $(document.createElement('span'));
+		writing.text(name);
+		thing.append(x);
+		thing.append(writing);
+		$('#filterlist').append(thing);
+		x.click(function() {
+			thing.remove();
+			activeFilters.delete(name);
+		})
+	}
+}
